0f19150fbbbc26dd0727ca2e79558962d3fd67f4
VIVEK <tailorvivekgec19@gmail.com>
2023-07-27 19:33:46 +0530

    ecommerce_app

--- lib/screens/04 first/addProduct/addProductScreen.dart
+++ lib/screens/04 first/addProduct/addProductScreen.dart
@@ -1,4 +1,6 @@
+import 'package:ecommerce_app/model/productModel.dart';
 import 'package:ecommerce_app/screens/04%20first/addProduct/addImageController.dart';
+import 'package:ecommerce_app/utils/firebase_helper.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:sizer/sizer.dart';
@@ -11,7 +13,6 @@ class AddProductScreen extends StatefulWidget {
 }
 
 class _AddProductScreenState extends State<AddProductScreen> {
-
   TextEditingController tname = TextEditingController();
   TextEditingController tdesc = TextEditingController();
   TextEditingController timgLink = TextEditingController();
@@ -23,103 +24,184 @@ class _AddProductScreenState extends State<AddProductScreen> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        appBar: AppBar(title: Text("Add Product"),centerTitle: true,elevation: 0,),
-
+        appBar: AppBar(
+          title: Text("Add Product"),
+          centerTitle: true,
+          elevation: 0,
+          backgroundColor: Colors.black,
+        ),
         body: Padding(
-          padding: EdgeInsets.symmetric(vertical: 2.h,horizontal: 5.w),
-          child: Column(
-            children: [
-              InkWell(onTap: () {
-                Get.defaultDialog(
-                  title: "Add Image",
-                  content: Column(
-                    children: [
-                      TextField(
-                        style: TextStyle(color: Color(0xff0A1172),fontSize: 16),
-                        keyboardType: TextInputType.url,
-                        controller: timgLink,
-                        decoration: InputDecoration(
-                            border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
-                            label: Text("Image Url",style: TextStyle(color: Color(0xff0A1172)),),
-                            hintText: "Enter Image Url here...",
-                            focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: Color(0xff0A1172),width: 1.3),borderRadius: BorderRadius.circular(10)),
-                            enabledBorder: OutlineInputBorder(borderSide: BorderSide(color: Color(0x550A1172),width: 1.3),borderRadius: BorderRadius.circular(10)),
-                            enabled: true
-
+          padding: EdgeInsets.symmetric(vertical: 2.h, horizontal: 5.w),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    Obx(
+                      () => Container(
+                        height: 15.h,
+                        width: 15.h,
+                        decoration: BoxDecoration(borderRadius: BorderRadius.circular(3.w),
+                        image: i_control.isAdded.value
+                            ?  DecorationImage(image: NetworkImage("${timgLink.text}"),fit: BoxFit.fill)
+                            :  DecorationImage(image: AssetImage("assets/images/add/picture.png"),fit: BoxFit.fill),
                         ),
-                      ),
-
-
-                      Padding(
-                        padding: EdgeInsets.symmetric(horizontal: 5.w),
-                        child: Row(mainAxisAlignment: MainAxisAlignment.spaceAround,
-                          children: [
-
-                          TextButton(onPressed: () {
-
-                            Get.back();
-                          }, child: Text("Done",style: TextStyle(color: Colors.blue),))
-                        ],),
                       )
-                    ],
-                  )
-                );
-              },
-                child: i_control.isAdded.value ? Image.network("${timgLink.text}",height: 10.h,width: 10.h) : Image.asset("assets/images/add/add.png",height: 10.h,width: 10.h,)),
-              SizedBox(height: 2.h,),
-              CustomTextField(controller: tname,hint: "Name",kboard:TextInputType.text),
-              Padding(
-                padding:  EdgeInsets.only(bottom:2.h),
-                child: TextField(
-                  textAlignVertical: TextAlignVertical.top,
-                  style: TextStyle(color: Color(0xff0A1172),fontSize: 16),
-                  keyboardType: TextInputType.streetAddress,
-                  controller: tdesc,
-                  maxLines: 3,
-                  decoration: InputDecoration(
-                      border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
-                      label: Text("Description",style: TextStyle(color: Color(0xff0A1172)),),
-                      hintText: "Enter Description here...",
-                      focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: Color(0xff0A1172),width: 1.3),borderRadius: BorderRadius.circular(10)),
-                      enabledBorder: OutlineInputBorder(borderSide: BorderSide(color: Color(0x550A1172),width: 1.3),borderRadius: BorderRadius.circular(10)),
-                      enabled: true
+                    ),
+                    SizedBox(width:10.w),
 
+                    TextButton(child:Text("Add Image"),onPressed: () {
+                      Get.defaultDialog(
+                          title: "Add Image",
+                          content: Column(
+                            children: [
+                              TextField(
+                                style: TextStyle(
+                                    color: Color(0xff0A1172), fontSize: 16),
+                                keyboardType: TextInputType.url,
+                                controller: timgLink,
+                                decoration: InputDecoration(
+                                    border: OutlineInputBorder(
+                                        borderRadius: BorderRadius.circular(10)),
+                                    label: Text(
+                                      "Image Url",
+                                      style: TextStyle(color: Color(0xff0A1172)),
+                                    ),
+                                    hintText: "Enter Image Url here...",
+                                    focusedBorder: OutlineInputBorder(
+                                        borderSide: BorderSide(
+                                            color: Color(0xff0A1172), width: 1.3),
+                                        borderRadius: BorderRadius.circular(10)),
+                                    enabledBorder: OutlineInputBorder(
+                                        borderSide: BorderSide(
+                                            color: Color(0x550A1172), width: 1.3),
+                                        borderRadius: BorderRadius.circular(10)),
+                                    enabled: true),
+                              ),
+                              Padding(
+                                padding: EdgeInsets.symmetric(horizontal: 5.w),
+                                child: Row(
+                                  mainAxisAlignment:
+                                  MainAxisAlignment.spaceAround,
+                                  children: [
+                                    TextButton(
+                                        onPressed: () {
+                                          i_control.isAdded.value = true;
+
+                                          print("img url link add is ==>>   ${timgLink.text}");
+
+                                          Get.back();
+                                        },
+                                        child: Text(
+                                          "Done",
+                                          style: TextStyle(color: Colors.blue),
+                                        ))
+                                  ],
+                                ),
+                              )
+                            ],
+                          ));
+                    },),
+                  ],
+                ),
+                SizedBox(height: 2.h),
+                CustomTextField(controller: tname, hint: "Name", kboard: TextInputType.text),
+                Padding(
+                  padding: EdgeInsets.only(bottom: 2.h),
+                  child: TextField(
+                    textAlignVertical: TextAlignVertical.top,
+                    style: TextStyle(color: Color(0xff0A1172), fontSize: 16),
+                    keyboardType: TextInputType.streetAddress,
+                    controller: tdesc,
+                    maxLines: 3,
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10)),
+                        label: Text(
+                          "Description",
+                          style: TextStyle(color: Color(0xff0A1172)),
+                        ),
+                        hintText: "Enter Description here...",
+                        focusedBorder: OutlineInputBorder(
+                            borderSide:
+                                BorderSide(color: Color(0xff0A1172), width: 1.3),
+                            borderRadius: BorderRadius.circular(10)),
+                        enabledBorder: OutlineInputBorder(
+                            borderSide:
+                                BorderSide(color: Color(0x550A1172), width: 1.3),
+                            borderRadius: BorderRadius.circular(10)),
+                        enabled: true),
                   ),
                 ),
-              ),
-              CustomTextField(controller: tprice,hint: "Price",kboard:TextInputType.number ),
+                CustomTextField(controller: tprice, hint: "Price", kboard: TextInputType.number),
+                SizedBox(height: 10.h),
 
+                GestureDetector(
+                  onTap: () {
+                    ProductModel model = ProductModel(
+                      name: tname.text,
+                      category: "Clothes",
+                      fav: false,
+                      img: timgLink.text,
+                      desc: tdesc.text,
+                      price: int.parse(tprice.text),
+                    );
 
+                    FirebaseHelper.firebaseHelper.addProductInFireStore(model);
 
-            ],
+                    tname.clear();
+                    tdesc.clear();
+                    timgLink.clear();
+                    tprice.clear();
+
+                    Get.back();
+
+                    print("product is add in fire cloud store");
+
+                    Get.snackbar('Product Added', "Your Product is added in The Firebase CloudStore",
+                        snackPosition: SnackPosition.BOTTOM,duration: Duration(seconds: 2));
+
+                  },
+                  child: Container(height: 7.h,width: 70.w,
+                  decoration: BoxDecoration(borderRadius: BorderRadius.circular(3.5.h),
+                    color: Colors.black),
+                    alignment: Alignment.center,
+
+                    child: Text("Add",style: TextStyle(color: Colors.white,fontSize: 16.sp,fontWeight: FontWeight.w400),),
+                  ),
+                )
+              ],
+            ),
           ),
         ),
-
       ),
     );
   }
 
-  Widget CustomTextField({controller,hint,kboard}) {
+  Widget CustomTextField({controller, hint, kboard}) {
     return Padding(
-      padding:  EdgeInsets.only(bottom:2.h),
+      padding: EdgeInsets.only(bottom: 2.h),
       child: TextField(
-        style: TextStyle(color: Color(0xff0A1172),fontSize: 16),
+        style: TextStyle(color: Color(0xff0A1172), fontSize: 16),
         keyboardType: kboard,
         controller: controller,
         decoration: InputDecoration(
             border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
-            label: Text("$hint",style: TextStyle(color: Color(0xff0A1172)),),
+            label: Text(
+              "$hint",
+              style: TextStyle(color: Color(0xff0A1172)),
+            ),
             hintText: "Enter $hint here...",
-            focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: Color(0xff0A1172),width: 1.3),borderRadius: BorderRadius.circular(10)),
-            enabledBorder: OutlineInputBorder(borderSide: BorderSide(color: Color(0x550A1172),width: 1.3),borderRadius: BorderRadius.circular(10)),
-            enabled: true
-
-        ),
+            focusedBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: Color(0xff0A1172), width: 1.3),
+                borderRadius: BorderRadius.circular(10)),
+            enabledBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: Color(0x550A1172), width: 1.3),
+                borderRadius: BorderRadius.circular(10)),
+            enabled: true),
       ),
     );
   }
-
 }
-
 
 //    https://cdn-icons-png.flaticon.com/512/5511/5511396.png
--- lib/utils/firebase_helper.dart
+++ lib/utils/firebase_helper.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:ecommerce_app/model/productModel.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class FirebaseHelper
@@ -9,7 +10,9 @@ class FirebaseHelper
   FirebaseAuth auth = FirebaseAuth.instance;
   FirebaseFirestore firestore = FirebaseFirestore.instance;
 
+//==========================================================================
 // =====================   Login  =========================================
+
   Future<String> emailSignUp({email,password})
   async {
 
@@ -34,8 +37,24 @@ class FirebaseHelper
     }
   }
 
-
+//==========================================================================
 // ========================= FireStore ===================================
+
+  void addProductInFireStore(ProductModel model)
+  {
+    String tempCategory = model.category!;
+    firestore.collection("ProductList").doc("Categories").collection(tempCategory).add(
+        {
+          "pname": model.name,
+          "pprice": model.price,
+          "pdesc":model.desc,
+          "pimg":model.img,
+          "pcategory":model.category,
+          "pfav":model.fav,
+    }
+    );
+
+  }
 
 
 
